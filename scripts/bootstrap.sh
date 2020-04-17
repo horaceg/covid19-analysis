a1ec265870edb00523b12abc49cc0603d4f8731d
Horace Guy <horace.guy.k@gmail.com>
2020-04-17 18:01:30 +0200

    install libpq-dev, wheel etc. before using pip

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
 conda install -c conda-forge --file requirements.txt
-pip install wheel
+sudo apt-get install gcc libpq-dev -y
+sudo apt-get install python3-dev python3-pip python3-venv python3-wheel -y
+pip3 install wheel
 pip install PyMuPDF
