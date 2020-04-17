3e57c7f0ff154d05df7450f9eaf4f67f794200ed
Horace Guy <horace.guy.k@gmail.com>
2020-04-17 18:07:00 +0200

    use pip from miniconda

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,7 +1,4 @@
 #! /bin/bash
 
 conda install -c conda-forge --file requirements.txt
-sudo apt-get install gcc libpq-dev -y
-sudo apt-get install python3-dev python3-pip python3-venv python3-wheel -y
-pip3 install wheel
-pip install PyMuPDF
+/usr/share/miniconda/bin/pip install PyMuPDF
